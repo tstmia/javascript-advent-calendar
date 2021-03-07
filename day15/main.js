69c284d38a5f0e65af8df6cd2321b6955ca20947
tstmia <tstmia0625@gmail.com>
2021-03-08 08:40:50 +0900

    challenge day15: fix carousel

--- day15/main.js
+++ day15/main.js
@@ -9,18 +9,24 @@ const changeCard = (btn) => {
     const currentCard = document.getElementsByClassName('current-card');
     const currentIndex = [].slice.call(cards).indexOf(currentCard[0]);
     const maxIndex = cards.length - 1;
+    let nextIndex = currentIndex + btn;
 
-    let index = currentIndex + btn;
-    if (index < 0) {
-        index = maxIndex;
-    } else if (index > maxIndex) {
-        index = 0;
+    const windowWidth = window.innerWidth;
+    const cardWidth = windowWidth * 0.5;
+
+    const images = document.getElementsByClassName('images')
+
+    if (nextIndex < 0) {
+        nextIndex = 3;
+    } else if (nextIndex > maxIndex) {
+        nextIndex = 0;
     }
 
     cards[currentIndex].classList.remove('current-card');
-    cards[index].classList.add('current-card');
+    cards[nextIndex].classList.add('current-card');
 
-}
+    images[0].style.transform = `translateX(${ cardWidth * nextIndex * -1}px)` ;
+};
 
 const previousBtn = document.getElementsByClassName('previous');
 const nextBtn  = document.getElementsByClassName('next');
