85735920046fb1342079f877cf34c8fee6ad8762
tstmia <tstmia0625@gmail.com>
2021-03-12 07:44:08 +0900

    challenge day21: sum of two

---
+++ day21/main.js
@@ -0,0 +1,42 @@
+/**
+ * day21
+ * sum of 2
+ * 
+ * @param {*} numArray1 
+ * @param {*} numArray2 
+ * @param {*} value 
+ * @returns true if a value can be found that by adding one number from each list
+ */
+const sumOfTow = (numArray1, numArray2, value) => {
+    for(let num1 of numArray1){
+        for(let num2 of numArray2){
+            if(num1 + num2 === value) return true;
+        }
+    }
+    return false;
+};
+
+/**
+ * modelAnswer
+ */
+ const modelAnswerOfsumOfTow = (numArray1, numArray2, value) => {
+     const map = {};
+    for(let num of numArray1){
+        const difference = value - num;
+        map[difference] = difference;
+    }
+    for(let num of numArray2){
+        // hasOwnProperty()はオブジェクト自身が指定されたプロパティを持っているかどうか真偽を返す
+        // map = { '39': 39, '40': 40, '41': 41 }
+        if(map.hasOwnProperty(num)) return true;
+    }
+    return false;
+};
+
+
+const a = [1, 2, 3];
+const b = [10, 20, 30, 40];
+const v = 42;
+
+console.log(sumOfTow(a, b, v));
+console.log(modelAnswerOfsumOfTow(a, b, v));
